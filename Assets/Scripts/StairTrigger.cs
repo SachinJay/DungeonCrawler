05148defbb9b31e18ec3aee7ead7d571e6421c53
Sachin Devasahayam <sjd232@cornell.edu>
2020-07-12 14:39:02 -0400

    Added new sprites
    Sprites for dialog box and dialog options

--- Assets/Scripts/StairTrigger.cs
+++ Assets/Scripts/StairTrigger.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class StairTrigger : MonoBehaviour
 {
     public string nextLevel;
 
+    public GameObject popUpBox;
+    public Animator animator;
+
+    public TMP_Text popUpMsg;
+
     //Triggered whenever stairs are entered
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        popUpBox.SetActive(true);
+        popUpMsg.text = "pop";
+        animator.SetTrigger("pop");
+
         //Load the next scene
         SceneManager.LoadScene(sceneName:nextLevel);
 
